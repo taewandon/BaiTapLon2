f103801690f8d5661f02308677c89cab56ea4e44
Phạm Hải Nam <phamhainam511@gmail.com>
2023-12-07 11:52:26 +0700

    Giao diện

---
+++ QUAN_LY_NGHIEP_VU.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTUD_với_CSDL
+{
+    public partial class QUAN_LY_NGHIEP_VU : Form
+    {
+        public QUAN_LY_NGHIEP_VU()
+        {
+            InitializeComponent();
+        }
+
+        private Form currentFormBody;
+        private void OpenFormBody(Form FormBody)
+        {
+            if (currentFormBody != null)
+            {
+                currentFormBody.Close();
+            }
+            currentFormBody = FormBody;
+            FormBody.TopLevel = false;
+            FormBody.FormBorderStyle = FormBorderStyle.None;
+            FormBody.Dock = DockStyle.Fill;
+            panel_Body.Controls.Add(FormBody);
+            panel_Body.Tag = FormBody;
+            FormBody.BringToFront();
+            FormBody.Show();
+        }
+
+        private void btn_DaoTao_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Dao_tao());
+        }
+
+        private void btn_CongTac_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Cong_tac());
+        }
+    }
+}
---
+++ QUAN_LY_NHAN_VIEN.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTUD_với_CSDL
+{
+    public partial class QUAN_LY_NHAN_VIEN : Form
+    {
+        public QUAN_LY_NHAN_VIEN()
+        {
+            InitializeComponent();
+        }
+
+        private Form currentFormBody;
+        private void OpenFormBody(Form FormBody)
+        {
+            if (currentFormBody != null)
+            {
+                currentFormBody.Close();
+            }
+            currentFormBody = FormBody;
+            FormBody.TopLevel = false;
+            FormBody.FormBorderStyle = FormBorderStyle.None;
+            FormBody.Dock = DockStyle.Fill;
+            panel_Body.Controls.Add(FormBody);
+            panel_Body.Tag = FormBody;
+            FormBody.BringToFront();
+            FormBody.Show();
+        }
+
+        private void btn_TTNV_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Thong_tin_nhan_vien());
+        }
+
+        private void btn_PB_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Phong_ban());
+        }
+
+        private void btn_BC_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Bang_cap());
+        }
+
+        private void btn_CC_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Chung_chi());
+        }
+
+        private void btn_HD_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Hop_dong());
+        }
+
+        private void btn_BH_Click(object sender, EventArgs e)
+        {
+            OpenFormBody(new Bao_hiem());
+        }
+    }
+}
