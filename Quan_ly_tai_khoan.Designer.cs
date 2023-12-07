0c3f3aadd1715e0f88d2f8022c60402a8b8c7578
Phạm Hải Nam <phamhainam511@gmail.com>
2023-12-07 11:12:46 +0700

    Quản lý tài khoản

---
+++ Quan_ly_tai_khoan.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PTUD_với_CSDL
+{
+    partial class Quan_ly_tai_khoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.DGV_HopDong = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_PQ = new System.Windows.Forms.TextBox();
+            this.txt_MK = new System.Windows.Forms.TextBox();
+            this.txt_TK = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_HopDong)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Sua.Location = new System.Drawing.Point(832, 189);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(161, 51);
+            this.btn_Sua.TabIndex = 28;
+            this.btn_Sua.Text = "Cập nhật";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            this.btn_Sua.Click += new System.EventHandler(this.btn_Sua_Click);
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Xoa.Location = new System.Drawing.Point(832, 272);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(161, 51);
+            this.btn_Xoa.TabIndex = 27;
+            this.btn_Xoa.Text = "Xóa thông tin";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            this.btn_Xoa.Click += new System.EventHandler(this.btn_Xoa_Click);
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Them.Location = new System.Drawing.Point(832, 107);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(161, 51);
+            this.btn_Them.TabIndex = 26;
+            this.btn_Them.Text = "Thêm thông tin";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.Click += new System.EventHandler(this.btn_Them_Click);
+            // 
+            // DGV_HopDong
+            // 
+            this.DGV_HopDong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_HopDong.Location = new System.Drawing.Point(185, 394);
+            this.DGV_HopDong.Name = "DGV_HopDong";
+            this.DGV_HopDong.RowHeadersWidth = 62;
+            this.DGV_HopDong.RowTemplate.Height = 28;
+            this.DGV_HopDong.Size = new System.Drawing.Size(808, 272);
+            this.DGV_HopDong.TabIndex = 25;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_PQ);
+            this.groupBox1.Controls.Add(this.txt_MK);
+            this.groupBox1.Controls.Add(this.txt_TK);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(185, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 277);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tài khoản";
+            // 
+            // txt_PQ
+            // 
+            this.txt_PQ.Location = new System.Drawing.Point(178, 198);
+            this.txt_PQ.Name = "txt_PQ";
+            this.txt_PQ.Size = new System.Drawing.Size(375, 33);
+            this.txt_PQ.TabIndex = 6;
+            // 
+            // txt_MK
+            // 
+            this.txt_MK.Location = new System.Drawing.Point(178, 120);
+            this.txt_MK.Name = "txt_MK";
+            this.txt_MK.Size = new System.Drawing.Size(258, 33);
+            this.txt_MK.TabIndex = 5;
+            // 
+            // txt_TK
+            // 
+            this.txt_TK.Location = new System.Drawing.Point(178, 40);
+            this.txt_TK.Name = "txt_TK";
+            this.txt_TK.Size = new System.Drawing.Size(152, 33);
+            this.txt_TK.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phân quyền:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài khoản:";
+            // 
+            // Quan_ly_tai_khoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.DGV_HopDong);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Quan_ly_tai_khoan";
+            this.Text = "Quan_ly_tai_khoan";
+            this.Load += new System.EventHandler(this.Quan_ly_tai_khoan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_HopDong)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.DataGridView DGV_HopDong;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_PQ;
+        private System.Windows.Forms.TextBox txt_MK;
+        private System.Windows.Forms.TextBox txt_TK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Quan_ly_tai_khoan.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTUD_với_CSDL
+{
+    public partial class Quan_ly_tai_khoan : Form
+    {
+        public Quan_ly_tai_khoan()
+        {
+            InitializeComponent();
+        }
+        SqlConnect data = new SqlConnect();
+        public void LoadTaiKhoan()
+        {
+            DGV_HopDong.DataSource = data.table("Select * From TaiKhoan");
+            DGV_HopDong.AutoResizeColumns();
+        }
+        public int CheckNhanVien(string ma)
+        {
+            string query = "select COUNT(*) from NhanVien where manv = '" + ma + "'";
+            return data.kiemtra(query);
+        }
+        public int CheckTaiKhoan(string ma)
+        {
+            string query = "select COUNT(*) from TaiKhoan where taikhoan = '" + ma + "'";
+            return data.kiemtra(query);
+        }
+        public void ResetTaiKhoan()
+        {
+            txt_TK.ResetText();
+            txt_MK.ResetText();
+            txt_PQ.ResetText();
+            txt_TK.Focus();
+        }
+
+        private void Quan_ly_tai_khoan_Load(object sender, EventArgs e)
+        {
+            LoadTaiKhoan();
+            txt_TK.Focus();
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_TK.Text) || string.IsNullOrEmpty(txt_MK.Text) || string.IsNullOrEmpty(txt_PQ.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin tài khoản");
+            }
+            else if (CheckNhanVien(txt_TK.Text) == 0)
+            {
+                MessageBox.Show("Tài khoản là mã nhân viên");
+            }
+            else if (CheckTaiKhoan(txt_TK.Text) == 1)
+            {
+                MessageBox.Show("Thông tin đã tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "insert into TaiKhoan values ('" + txt_TK.Text + "',N'" + txt_MK.Text + "',N'" + txt_PQ.Text + "')";
+                    data.command(query);
+                    LoadTaiKhoan();
+                    MessageBox.Show("Thêm thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetTaiKhoan();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_TK.Text) || string.IsNullOrEmpty(txt_MK.Text) || string.IsNullOrEmpty(txt_PQ.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin tài khoản");
+            }
+            else if (CheckTaiKhoan(txt_TK.Text) == 0)
+            {
+                MessageBox.Show("Tài khoản không tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "update TaiKhoan set matkhau = N'" + txt_MK.Text + "',phanquyen = N'" + txt_PQ.Text + "' where taikhoan = '" + txt_TK.Text + "'";
+                    data.command(query);
+                    LoadTaiKhoan();
+                    MessageBox.Show("Sửa thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetTaiKhoan();
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_TK.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tài khoản");
+            }
+            else if (CheckTaiKhoan(txt_TK.Text) == 0)
+            {
+                MessageBox.Show("Tài khoản không tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "delete from TaiKhoan where taikhoan = '" + txt_TK.Text + "'";
+                    data.command(query);
+                    LoadTaiKhoan();
+                    MessageBox.Show("Xóa thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetTaiKhoan();
+        }
+    }
+}
