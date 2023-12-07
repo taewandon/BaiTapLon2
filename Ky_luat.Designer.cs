ffaf242cc132a6451d17cd13e6c291658d25917a
Phạm Hải Nam <phamhainam511@gmail.com>
2023-12-07 10:03:22 +0700

    Khen thưởng, Kỷ luật

---
+++ Ky_luat.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace PTUD_với_CSDL
+{
+    partial class Ky_luat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Xoa2 = new System.Windows.Forms.Button();
+            this.btn_Them2 = new System.Windows.Forms.Button();
+            this.DGV_NhanVien = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbb_KL = new System.Windows.Forms.ComboBox();
+            this.txt_MNV = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btn_Sua1 = new System.Windows.Forms.Button();
+            this.btn_Xoa1 = new System.Windows.Forms.Button();
+            this.btn_Them1 = new System.Windows.Forms.Button();
+            this.DGV_KyLuat = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_TP = new System.Windows.Forms.TextBox();
+            this.txt_TKL = new System.Windows.Forms.TextBox();
+            this.txt_MKL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_NhanVien)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_KyLuat)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Xoa2
+            // 
+            this.btn_Xoa2.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Xoa2.Location = new System.Drawing.Point(963, 321);
+            this.btn_Xoa2.Name = "btn_Xoa2";
+            this.btn_Xoa2.Size = new System.Drawing.Size(130, 50);
+            this.btn_Xoa2.TabIndex = 50;
+            this.btn_Xoa2.Text = "Xóa";
+            this.btn_Xoa2.UseVisualStyleBackColor = true;
+            this.btn_Xoa2.Click += new System.EventHandler(this.btn_Xoa2_Click);
+            // 
+            // btn_Them2
+            // 
+            this.btn_Them2.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Them2.Location = new System.Drawing.Point(733, 321);
+            this.btn_Them2.Name = "btn_Them2";
+            this.btn_Them2.Size = new System.Drawing.Size(130, 50);
+            this.btn_Them2.TabIndex = 49;
+            this.btn_Them2.Text = "Thêm";
+            this.btn_Them2.UseVisualStyleBackColor = true;
+            this.btn_Them2.Click += new System.EventHandler(this.btn_Them2_Click);
+            // 
+            // DGV_NhanVien
+            // 
+            this.DGV_NhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_NhanVien.Location = new System.Drawing.Point(673, 406);
+            this.DGV_NhanVien.Name = "DGV_NhanVien";
+            this.DGV_NhanVien.RowHeadersWidth = 62;
+            this.DGV_NhanVien.RowTemplate.Height = 28;
+            this.DGV_NhanVien.Size = new System.Drawing.Size(462, 300);
+            this.DGV_NhanVien.TabIndex = 48;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbb_KL);
+            this.groupBox2.Controls.Add(this.txt_MNV);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(673, 39);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(462, 250);
+            this.groupBox2.TabIndex = 47;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nhân viên";
+            // 
+            // cbb_KL
+            // 
+            this.cbb_KL.FormattingEnabled = true;
+            this.cbb_KL.Location = new System.Drawing.Point(196, 149);
+            this.cbb_KL.Name = "cbb_KL";
+            this.cbb_KL.Size = new System.Drawing.Size(224, 33);
+            this.cbb_KL.TabIndex = 6;
+            // 
+            // txt_MNV
+            // 
+            this.txt_MNV.Location = new System.Drawing.Point(196, 63);
+            this.txt_MNV.Name = "txt_MNV";
+            this.txt_MNV.Size = new System.Drawing.Size(178, 33);
+            this.txt_MNV.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(28, 157);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(114, 25);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Mã kỷ luật:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(28, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(136, 25);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Mã nhân viên:";
+            // 
+            // btn_Sua1
+            // 
+            this.btn_Sua1.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Sua1.Location = new System.Drawing.Point(256, 321);
+            this.btn_Sua1.Name = "btn_Sua1";
+            this.btn_Sua1.Size = new System.Drawing.Size(130, 50);
+            this.btn_Sua1.TabIndex = 46;
+            this.btn_Sua1.Text = "Sửa";
+            this.btn_Sua1.UseVisualStyleBackColor = true;
+            this.btn_Sua1.Click += new System.EventHandler(this.btn_Sua1_Click);
+            // 
+            // btn_Xoa1
+            // 
+            this.btn_Xoa1.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Xoa1.Location = new System.Drawing.Point(484, 321);
+            this.btn_Xoa1.Name = "btn_Xoa1";
+            this.btn_Xoa1.Size = new System.Drawing.Size(130, 50);
+            this.btn_Xoa1.TabIndex = 45;
+            this.btn_Xoa1.Text = "Xóa";
+            this.btn_Xoa1.UseVisualStyleBackColor = true;
+            this.btn_Xoa1.Click += new System.EventHandler(this.btn_Xoa1_Click);
+            // 
+            // btn_Them1
+            // 
+            this.btn_Them1.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.btn_Them1.Location = new System.Drawing.Point(48, 321);
+            this.btn_Them1.Name = "btn_Them1";
+            this.btn_Them1.Size = new System.Drawing.Size(130, 50);
+            this.btn_Them1.TabIndex = 44;
+            this.btn_Them1.Text = "Thêm";
+            this.btn_Them1.UseVisualStyleBackColor = true;
+            this.btn_Them1.Click += new System.EventHandler(this.btn_Them1_Click);
+            // 
+            // DGV_KyLuat
+            // 
+            this.DGV_KyLuat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_KyLuat.Location = new System.Drawing.Point(48, 406);
+            this.DGV_KyLuat.Name = "DGV_KyLuat";
+            this.DGV_KyLuat.RowHeadersWidth = 62;
+            this.DGV_KyLuat.RowTemplate.Height = 28;
+            this.DGV_KyLuat.Size = new System.Drawing.Size(566, 300);
+            this.DGV_KyLuat.TabIndex = 43;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_TP);
+            this.groupBox1.Controls.Add(this.txt_TKL);
+            this.groupBox1.Controls.Add(this.txt_MKL);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(48, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 250);
+            this.groupBox1.TabIndex = 42;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kỷ luật";
+            // 
+            // txt_TP
+            // 
+            this.txt_TP.Location = new System.Drawing.Point(208, 167);
+            this.txt_TP.Name = "txt_TP";
+            this.txt_TP.Size = new System.Drawing.Size(178, 33);
+            this.txt_TP.TabIndex = 16;
+            // 
+            // txt_TKL
+            // 
+            this.txt_TKL.Location = new System.Drawing.Point(208, 105);
+            this.txt_TKL.Name = "txt_TKL";
+            this.txt_TKL.Size = new System.Drawing.Size(309, 33);
+            this.txt_TKL.TabIndex = 15;
+            // 
+            // txt_MKL
+            // 
+            this.txt_MKL.Location = new System.Drawing.Point(208, 43);
+            this.txt_MKL.Name = "txt_MKL";
+            this.txt_MKL.Size = new System.Drawing.Size(178, 33);
+            this.txt_MKL.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Mã kỷ luật:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tiền phạt:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tên kỷ luật:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 3;
+            // 
+            // Ky_luat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.Controls.Add(this.btn_Xoa2);
+            this.Controls.Add(this.btn_Them2);
+            this.Controls.Add(this.DGV_NhanVien);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btn_Sua1);
+            this.Controls.Add(this.btn_Xoa1);
+            this.Controls.Add(this.btn_Them1);
+            this.Controls.Add(this.DGV_KyLuat);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Ky_luat";
+            this.Text = "Ky_luat";
+            this.Load += new System.EventHandler(this.Ky_luat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_NhanVien)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_KyLuat)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Xoa2;
+        private System.Windows.Forms.Button btn_Them2;
+        private System.Windows.Forms.DataGridView DGV_NhanVien;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_MNV;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btn_Sua1;
+        private System.Windows.Forms.Button btn_Xoa1;
+        private System.Windows.Forms.Button btn_Them1;
+        private System.Windows.Forms.DataGridView DGV_KyLuat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_TP;
+        private System.Windows.Forms.TextBox txt_TKL;
+        private System.Windows.Forms.TextBox txt_MKL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbb_KL;
+    }
+}
---
+++ Ky_luat.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTUD_với_CSDL
+{
+    public partial class Ky_luat : Form
+    {
+        public Ky_luat()
+        {
+            InitializeComponent();
+        }
+        SqlConnect data = new SqlConnect();
+
+        public void LoadKyLuat()
+        {
+            DGV_KyLuat.DataSource = data.table("Select * From KyLuat");
+            DGV_KyLuat.AutoResizeColumns();
+        }
+        public void LoadNhanVien()
+        {
+            DGV_NhanVien.DataSource = data.table("Select manv, tenkyluat From NhanVien_KyLuat inner join KyLuat on NhanVien_KyLuat.makyluat = KyLuat.makyluat");
+            DGV_NhanVien.AutoResizeColumns();
+        }
+        public void LoadComboBox()
+        {
+            cbb_KL.DataSource = data.table("Select * From KyLuat");
+            cbb_KL.DisplayMember = "tenkyluat";
+            cbb_KL.ValueMember = "makyluat";
+        }
+
+        public int CheckKyLuat(string ma)
+        {
+            string query = "select COUNT(*) from KyLuat where makyluat = '" + ma + "'";
+            return data.kiemtra(query);
+        }
+        public int CheckNhanVien(string ma)
+        {
+            string query = "select COUNT(*) from NhanVien where manv = '" + ma + "'";
+            return data.kiemtra(query);
+        }
+        public int Check_NVKL(string manv, string makl)
+        {
+            string query = "select COUNT(*) from NhanVien_KyLuat where manv = '" + manv + "' and makyluat = '" + makl + "'";
+            return data.kiemtra(query);
+        }
+
+        public void ResetKyLuat()
+        {
+            txt_MKL.ResetText();
+            txt_TKL.ResetText();
+            txt_TP.ResetText();
+            txt_MKL.Focus();
+        }
+        public void ResetNhanVien()
+        {
+            txt_MNV.ResetText();
+            cbb_KL.SelectedIndex = 0;
+            txt_MNV.Focus();
+        }
+
+        private void btn_Them1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_MKL.Text) || string.IsNullOrEmpty(txt_TKL.Text) || string.IsNullOrEmpty(txt_TP.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin kỷ luật");
+            }
+            else if (CheckKyLuat(txt_MKL.Text) == 1)
+            {
+                MessageBox.Show("Mã kỷ luật đã tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "insert into KyLuat values ('" + txt_MKL.Text + "',N'" + txt_TKL.Text + "'," + int.Parse(txt_TP.Text) + ")";
+                    data.command(query);
+                    LoadKyLuat();
+                    LoadComboBox();
+                    MessageBox.Show("Thêm thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetKyLuat();
+        }
+
+        private void btn_Sua1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_MKL.Text) || string.IsNullOrEmpty(txt_TKL.Text) || string.IsNullOrEmpty(txt_TP.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin kỷ luật");
+            }
+            else if (CheckKyLuat(txt_MKL.Text) == 0)
+            {
+                MessageBox.Show("Mã kỷ luật không tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "update KyLuat set tenkyluat = N'" + txt_TKL.Text + "', tienphat = " + int.Parse(txt_TP.Text) + " where makyluat ='" + txt_MKL.Text + "'";
+                    data.command(query);
+                    LoadKyLuat();
+                    LoadComboBox();
+                    MessageBox.Show("Sửa thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetKyLuat();
+        }
+
+        private void btn_Xoa1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_MKL.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mã kỷ luật");
+            }
+            else if (CheckKyLuat(txt_MKL.Text) == 0)
+            {
+                MessageBox.Show("Mã kỷ luật không tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "delete from KyLuat where makyluat = '" + txt_MKL.Text + "'";
+                    data.command(query);
+                    LoadKyLuat();
+                    LoadComboBox();
+                    MessageBox.Show("Xóa thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetKyLuat();
+        }
+
+        private void btn_Them2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_MNV.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ mã nhân viên");
+            }
+            else if (CheckNhanVien(txt_MNV.Text) == 0)
+            {
+                MessageBox.Show("Mã nhân viên đã tồn tại");
+            }
+            else if (Check_NVKL(txt_MNV.Text, cbb_KL.SelectedValue.ToString()) == 1)
+            {
+                MessageBox.Show("Thông tin đã tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "insert into NhanVien_KyLuat values ('" + txt_MNV.Text + "','" + cbb_KL.SelectedValue.ToString() + "')";
+                    data.command(query);
+                    LoadNhanVien();
+                    MessageBox.Show("Thêm thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetNhanVien();
+        }
+
+        private void btn_Xoa2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_MNV.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mã nhân viên");
+            }
+            else if (CheckNhanVien(txt_MNV.Text) == 0)
+            {
+                MessageBox.Show("Mã nhân viên không tồn tại");
+            }
+            else if (Check_NVKL(txt_MNV.Text, cbb_KL.SelectedValue.ToString()) == 0)
+            {
+                MessageBox.Show("Thông tin cần xóa không tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    string query = "delete from NhanVien_KyLuat where manv = '" + txt_MNV.Text + "' and makyluat = '" + cbb_KL.SelectedValue + "'";
+                    data.command(query);
+                    LoadNhanVien();
+                    MessageBox.Show("Xóa thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Loi: " + ex.Message);
+                }
+            }
+            ResetNhanVien();
+        }
+
+        private void Ky_luat_Load(object sender, EventArgs e)
+        {
+            LoadKyLuat();
+            LoadNhanVien();
+            LoadComboBox();
+        }
+    }
+}
