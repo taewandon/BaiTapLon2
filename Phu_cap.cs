24265bae4e999765aa8868416184b492fb3b1559
taquangdong <96369203+buihoangnghi@users.noreply.github.com>
2023-12-07 12:29:42 +0700

    Tạ Quang Đồng
    các form: bảng lương, bảo hiểm, phụ cấp

---
+++ Phu_cap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTUD_với_CSDL
+{
+    public partial class Phu_cap : Form
+    {
+        public Phu_cap()
+        {
+            InitializeComponent();
+        }
+    }
+}
